e98d7e2e73c94091ea89bf18e582f59412879c0f
Nitesh Kumar M <65917611+niteshkumarm287@users.noreply.github.com>
2024-01-12 12:49:09 +0530

    Update new monitor.tf

--- new monitor.tf
+++ new monitor.tf
@@ -1,17 +1,1 @@
-resource "datadog_monitor" "foo" {
-  name               = "Name for monitor foo"
-  type               = "metric alert"
-  message            = "Monitor triggered. Notify: @hipchat-channel"
-  escalation_message = "Escalation message @pagerduty"
 
-  query = "avg(last_1h):avg:aws.ec2.cpu{environment:foo,host:foo} by {host} > 4"
-
-  monitor_thresholds {
-    warning  = 2
-    critical = 4
-  }
-
-  include_tags = true
-
-  tags = ["foo:bar", "team:fooBar"]
-}
